effa020b280fc8d642af96d6cfacb6cf09234008
Pewi1234 <rohidhbears2005@gmail.com>
2022-09-01 02:49:15 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,6 +3,8 @@ function formDone() {
   var airconTime = parseInt(document.getElementById("aircon").value);
   var showerTime = parseInt(document.getElementById("shower").value);
   var lightTime = parseInt(document.getElementById("lights").value);
+  var fanTime = parseInt(document.getElementById("fans").value); //time fan left on
+  var fanNumber = parseInt(document.getElementById("fan-number").value); //num of fans
 
   //Aircon calculate 1ac-> 2.5kw
   var airconCost = 2.5 * airconTime * cost;
@@ -21,4 +23,9 @@ function formDone() {
   var lightCost = lightEnergy * cost;
   lightCost = lightCost.toFixed(2);
   console.log(lightCost);
+
+  //fan calculate - each fan 70 watts
+  var fanEnergy = 70/1000 * fanTime * cost * fanNumber;
+  fanEnergy = fanEnergy.toFixed(2);
+  console.log(fanEnergy);
 }
