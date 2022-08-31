34cb81944472de4ce3422b3e235c81c85572b852
Pewi1234 <rohidhbears2005@gmail.com>
2022-09-01 01:08:57 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,4 +5,12 @@ function formDone() {
   //Aircon calculate 1ac-> 2.5kw
   var airconConsump = (2.5 * airconTime * 32.28) / 100;
   console.log(airconConsump);
+
+  //shower calculate
+  var showerCo2 = showerTime * 0.204;
+  console.log(showerCo2);
+
+  var showerEnergy = showerTime * 0.168;
+  showerEnergy = showerEnergy.toFixed(2);
+  console.log(showerEnergy);
 }
