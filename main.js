7ef131adfdf625af4dee2a10952610bd0f464a38
xavierwoon-cmd <111954471+xavierwoon-cmd@users.noreply.github.com>
2022-09-01 23:44:13 +0800

    xav

--- main.js
+++ main.js
@@ -11,18 +11,18 @@ window.addEventListener('scroll', () =>{
     navbar.classList.remove('nav-toggle');
 });
 
-function pledge(){
-        //var name = document.getElementsByName("name");
-        var name = document.getElementById('name').value;
-        var message =  document.getElementById('message').value;
-        //let name = document.createElement("p");
-        //name.style.color = "darkgreen";
-        //name.style.weight = "bold";
-        let ele = document.createElement("p");
-        let na = document.createElement("p");
-        na.innerText = name;
-        na.style.color = "green";
-        ele.innerText = message;
-        document.getElementById('pledge').appendChild(na);
-        document.getElementById('pledge').appendChild(ele);
-        }
+// function pledge(){
+//         //var name = document.getElementsByName("name");
+//         var name = document.getElementById('name').value;
+//         var message =  document.getElementById('message').value;
+//         //let name = document.createElement("p");
+//         //name.style.color = "darkgreen";
+//         //name.style.weight = "bold";
+//         let ele = document.createElement("p");
+//         let na = document.createElement("p");
+//         na.innerText = name;
+//         na.style.color = "green";
+//         ele.innerText = message;
+//         document.getElementById('pledge').appendChild(na);
+//         document.getElementById('pledge').appendChild(ele);
+//         }
